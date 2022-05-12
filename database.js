463b86fb2dee43f87331d9f9da655e3ec8a05404
UP2011524 <76070429+UP2011524@users.noreply.github.com>
2022-05-12 19:01:57 +0100

    changed to select only the word

--- database.js
+++ database.js
@@ -10,10 +10,10 @@ sql.on('error', (err) => {
 });
 
 export async function selectWord(id) {
-  const q = 'SELECT * FROM wordsTable WHERE id = $1;';
+  const q = 'SELECT wprds FROM wordsTable WHERE id = $1;';
   const result = await sql.query(q, [id]);
   console.log('from db.js');
-  console.log(result.rows[0]);
-  console.log(result);
+  // console.log(result.rows[0]);
+  // console.log(result);
   return result.rows[0];
 }
