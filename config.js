fc2a9fb9ced5b3bffd4ae03bddb614b41a7e6f1e
UP2011524 <76070429+UP2011524@users.noreply.github.com>
2022-05-12 18:15:29 +0100

    missed config line

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   // remove the next line if not running on our VM and connection to PostgreSQL doesn't work
-  // host: '/var/run/postgresql',
+  host: '/var/run/postgresql',
 
   database: 'wordle',
   statement_timeout: 5000,
